6bdfed5cbf7523607bb2a5cf8fde91eb5baf8aa6
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2020-09-24 00:34:48 +0800

    Update MP

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/CharacterSync.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/CharacterSync.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87f6a95b363441e9b3cde42e78f95529deae042859e84364bff1285f07733b6a
-size 53519
+oid sha256:79bd434cffec04e7f107d0f105f57741dd1a8c023de9428378badccd82faf62a
+size 53485
