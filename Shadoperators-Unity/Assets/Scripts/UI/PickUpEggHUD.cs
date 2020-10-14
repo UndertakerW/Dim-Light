8de0fac045b3f8f2c8b508bf55c1c0fc9766b502
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2020-10-14 17:07:22 +0800

    Adjust UI

--- Shadoperators-Unity/Assets/Scripts/UI/PickUpEggHUD.cs
+++ Shadoperators-Unity/Assets/Scripts/UI/PickUpEggHUD.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55e7a03d4489eda58449d53e56aeafc3b4d27240043e2bcf9c8c7164d429596a
-size 1380
+oid sha256:e5b2bf0b65dcca68c0261ac0f4bf1f2b7e0721a5a263ad56874f94f84e488457
+size 1264
