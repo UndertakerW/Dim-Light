06bf47185666e7b2d4fe6f1b587973998bb22fc8
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2020-10-16 04:42:13 +0800

    Update MP

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Controller/InventoryManager.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Controller/InventoryManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd35f713060edd246d7ee84673cdaefd82d340a396c352793e1e3aa96cdbe967
-size 119605
+oid sha256:d97b25f21c320a25858d5cda800e727859c481b5e47aed32657418a12902e003
+size 119880
