1d1bb16e5aa8e3786e9c51c073ea7c063d0d6893
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2020-10-27 00:39:04 +0800

    Update Characters & Weapons & Lobby

--- Shadoperators-Unity/Assets/Scripts/UI/TempCharacterIcon.cs
+++ Shadoperators-Unity/Assets/Scripts/UI/TempCharacterIcon.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a75af797eb0d60345c130ee4a4c3c88b1652d346d9ce139b27e35575f1f5c6d
-size 577
+oid sha256:90acf89d6d420b900ee703eb62c351e9803f2703418610c4a158eedc7bc09d82
+size 1481
