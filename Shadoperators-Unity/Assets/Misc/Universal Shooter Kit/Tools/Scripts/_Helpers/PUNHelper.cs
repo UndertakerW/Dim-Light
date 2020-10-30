37cf99e58881a494ddb6d60a643ceb0c69cf9d34
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2020-10-30 19:25:07 +0800

    Update Target Text & KD Info

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Tools/Scripts/_Helpers/PUNHelper.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Tools/Scripts/_Helpers/PUNHelper.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c067a90c9907e0ac465354c9054e60fd5fae7cbd68d4e23e8d19a58d3cfe7fb4
-size 41924
+oid sha256:f42a443c4dca7b9daba942d9c824a8b0e65669524618de4f5535430d4dfe69aa
+size 41928
---
+++ Shadoperators-Unity/Assets/Scripts/UI/KillDeathInfo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c50f15829e7efd35e551926f4d87fd0d3a54e995a83bad3ffba977dcd62e6e8
+size 502
