3cd363629abc7b228225f1cb90dde055eea4b94e
MoyuST <46509809+MoyuST@users.noreply.github.com>
2021-01-14 23:13:27 +0800

    add waiting room
    add a new scene called waiting room which makes sure that the scene will only be loaded when the game starts

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/Lobby.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/Lobby.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:557b7cbad67c0f1e2e1b9b74f8f1646f059abe240f817b532f41d3ff291f2e9e
-size 65663
+oid sha256:dfa38790b7baf9ecc166edd9b38d584ab45259c9ca62252b62ecad6eb797175c
+size 65815
--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6c3caa5fc6c992441c0f965fe4c9336d1182a431738dcb98af80a1077dee9bc
-size 115745
+oid sha256:cbc5ca68fcc474d3fa488cc73d6d4e208c886304c280eded3574053f932dc304
+size 117273
