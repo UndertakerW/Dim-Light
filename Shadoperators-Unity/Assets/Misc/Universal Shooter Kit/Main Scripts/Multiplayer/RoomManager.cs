62bc6bc5f79ebf0ea05761be0bab7fd2d5c33183
MoyuST <46509809+MoyuST@users.noreply.github.com>
2021-01-16 01:07:07 +0800

    Update RoomManager.cs
    update camera position

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d99261828de53619bab046ec7849884f147c27248d75b76bcb1f75b3d812beb
-size 117835
+oid sha256:61646ee0f39b2ab83c928b9f87b839b80f5591b67cf1bd0a938d65a7ae14603d
+size 118217
