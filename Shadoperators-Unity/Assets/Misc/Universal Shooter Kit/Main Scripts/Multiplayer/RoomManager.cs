3958937af98f6d2dac3ff2d329f21a1b75ff02ed
MoyuST <46509809+MoyuST@users.noreply.github.com>
2021-01-21 00:33:38 +0800

    update playericon
    1. set the icon object into the roommanager.cs
    2. players can only see their own icon

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ba0bd3ad60584da07646b5070e669bb8ca169ccc5c8365d7e6b93cffd2c544e
-size 118872
+oid sha256:344e57c01047cc630bb635dea61c7a8a3c9256c0b04ff4697539a75f64b8bb6e
+size 119094
--- Shadoperators-Unity/Assets/SetLayer.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:40217043999b395d201b77c8485ff0bcd6095fa85781f25f86d73f585a4101b6
-size 370
