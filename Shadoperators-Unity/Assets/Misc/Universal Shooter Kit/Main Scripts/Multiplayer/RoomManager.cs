6d9abb00c277bb4e00595c6871c8155c86d3c812
Sylviachelsea <43447503+Sylviachelsea@users.noreply.github.com>
2021-01-28 17:23:44 +0800

    rotation angle

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9740787cdc7d13e515bcf2ddf8054b49ebd0b6885c71e74ce3a0e4bddea1b7c6
+oid sha256:17de28a74f84cfe18aaedeaa764b8af91ab9b83473cb663ecb13493c4fa9a00a
 size 121579
