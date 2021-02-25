bb3d0570140954f993cc413ab423457254bff9c1
MoyuST <46509809+MoyuST@users.noreply.github.com>
2021-02-25 14:32:32 +0800

    modify healthkit
    1. modify the healthkit prefab to make it synchronize
    2. update the pick up menu in characterUI

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Controller/InventoryManager.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Controller/InventoryManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f447e1cdc91408b039c02f8d023db98ce13ab6f26ad0eb90778941467b0997b5
-size 120761
+oid sha256:167d3c644632f6fb83e0e2629aafa4992af5ac8a491790f4440e8f1ad3c6dbe5
+size 121104
--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Other/PickUp.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Other/PickUp.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b64d66bb57f6ce0102bfadd7fdaed1c6ed130fed2f019e0984a9726fdbf98ad8
-size 12209
+oid sha256:edee0a46d04a078452ca22dcdfa264099262dda57e2d51534ca108c3527c2dee
+size 12427
