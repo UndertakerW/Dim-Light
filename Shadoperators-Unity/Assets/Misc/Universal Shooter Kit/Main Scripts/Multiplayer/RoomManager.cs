16fb06b891a17a420cc53679df440a601fa6cbb3
MoyuST <46509809+MoyuST@users.noreply.github.com>
2021-02-27 15:56:56 +0800

    add function to "Mid" player
    the "Mid" player can press "M" to drop a health kit

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c89636dd2ea1c7a42e54b2f4a988712d676ec9569a20ff73829662516e7f25e3
-size 126764
+oid sha256:ab5796f92eee2f228ab52b73cf7842ddc12c5611fef6c15af549585dc31d5d0c
+size 127615
