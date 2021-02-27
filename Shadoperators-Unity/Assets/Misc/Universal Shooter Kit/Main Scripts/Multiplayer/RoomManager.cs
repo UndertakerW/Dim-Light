cc6e77ef469ff7fdbec8608ffc20822d352f8776
MoyuST <46509809+MoyuST@users.noreply.github.com>
2021-02-27 15:04:55 +0800

    fix a little bug in icons
    fix a little bug in icons

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18d645c1c200cb3e8388ad6b39a62808cf1456cb4e049b5428de3d071f44ea3d
-size 126555
+oid sha256:c89636dd2ea1c7a42e54b2f4a988712d676ec9569a20ff73829662516e7f25e3
+size 126764
