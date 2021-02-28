922be19f9a273dc68fa8e3246a9e9adef9e0cfce
Sylviachelsea <43447503+Sylviachelsea@users.noreply.github.com>
2021-02-28 09:22:21 +0800

    lightoff update half

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab5796f92eee2f228ab52b73cf7842ddc12c5611fef6c15af549585dc31d5d0c
-size 127615
+oid sha256:9cefa1fac35d8966e1cdd4fb4ef667448d61b1e6d206479cfd7e3d6b7c89fe42
+size 128060
