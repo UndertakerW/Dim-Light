724d5e5a4dfd1b7d1d3c0f2f00929483af4532dc
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2021-03-01 13:37:01 +0800

    Fix NullReference Error in Main Menu

--- Shadoperators-Unity/Assets/Scripts/Character/EggBag.cs
+++ Shadoperators-Unity/Assets/Scripts/Character/EggBag.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93cd754ba75ad06b01e3378de4eb618d648b8dec9286c02f356f507d9f54bce8
-size 2154
+oid sha256:0a295bdb385bce38daaaee526e963f1e1a5545741a3482494ba2adaba628c942
+size 2212
