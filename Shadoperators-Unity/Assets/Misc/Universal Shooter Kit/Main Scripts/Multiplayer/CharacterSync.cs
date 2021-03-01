ec6dd08bae05989ee789e4b84c3040b2664c1f69
Sylviachelsea <43447503+Sylviachelsea@users.noreply.github.com>
2021-03-01 17:40:25 +0800

    LightSync

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/CharacterSync.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/CharacterSync.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad5df07ffbfa110d33c82c138ce889575b0ff318e131b2c7c4e602571c63772d
-size 55363
+oid sha256:58b37515ac71ebf8d663947f3fab56c237d08b4974da8046b6689b9352e21f68
+size 56289
