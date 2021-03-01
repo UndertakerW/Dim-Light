8220e2eed8a253f698adbe833a8c8c637259ae3e
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2021-03-02 02:20:06 +0800

    Fix 4 kinds of errors

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f9779bef3a41df16828cbcb1d8f471a67cd3d6c2526bcf4dd6878fed87acbf3
-size 127622
+oid sha256:19d45cc15e6fffbb612156c21a7d2434f6700824bd3ea1a8ef2f46590094523e
+size 127877
