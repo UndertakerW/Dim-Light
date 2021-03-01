84d301c5cbcdefbc714950521c94e5424fcf7266
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2021-03-01 17:52:17 +0800

    Update Camera Position

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Controller/CameraController.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Controller/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:359c92eb6cbfd386d33bc419901e1a5b47d60c5330ffb26d9a71a71b0c19ccb9
+oid sha256:ab064b8d2fe0c41a0dfa8be45df5aecdddebe2bdb35309e5fb4c435d324ff3be
 size 53579
