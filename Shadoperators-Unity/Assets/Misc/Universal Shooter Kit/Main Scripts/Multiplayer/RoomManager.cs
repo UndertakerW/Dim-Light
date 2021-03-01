a661b67a58039857117abe3e501444071f25307e
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2021-03-01 14:07:09 +0800

    Update Waiting Room (Allow more than 5 players per team)

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ec5d42b823b86af4c33e99290b421efceae39658c0b177561206c2c5fc1e305
-size 128887
+oid sha256:31c726e8b5dd261b14ae5f8b7173d702cde04a569ba293199421208c2fec7b79
+size 128892
