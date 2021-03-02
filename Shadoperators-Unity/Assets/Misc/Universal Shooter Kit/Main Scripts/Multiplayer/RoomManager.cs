850714505686addbb808f2f6b6bd51f7b72614fb
Sylviachelsea <43447503+Sylviachelsea@users.noreply.github.com>
2021-03-02 20:24:58 +0800

    DELETE USELESS LINES

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/RoomManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19d45cc15e6fffbb612156c21a7d2434f6700824bd3ea1a8ef2f46590094523e
-size 127877
+oid sha256:566cd354d03596f0e0dc81b798f4247f67d03eedefc7d1e503f81895eaf96a65
+size 127564
