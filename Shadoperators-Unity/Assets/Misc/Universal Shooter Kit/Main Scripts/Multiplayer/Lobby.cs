9f021ed4442bf728158e80ec25efd33edd241ae3
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2021-03-03 01:26:51 +0800

    Update Lobby

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/Lobby.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/Lobby.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b1270d4b864327dcba53c0641652ad5a65e516b9fc894e9f9197f1c5b4808b7
-size 65902
+oid sha256:605fca3c2a49a0bf8cf70a75b25d614abe838c9db62bf143364e7a498785bf86
+size 65946
--- Shadoperators-Unity/Assets/Scripts/Teamlist.cs
+++ Shadoperators-Unity/Assets/Scripts/Teamlist.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8163bc39dae0a176ba25d8fb77e8a6d7fa3744230249b7df946fd9c6b2da6320
-size 5628
+oid sha256:823e0b8a887270fefc0e30144aee9a6e2a2da6883c4c68a5dd5258c8477ee07c
+size 5623
