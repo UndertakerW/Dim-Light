6d159ee6ea646e927a5d512396210bfd680ce1ac
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2021-03-04 20:37:09 +0800

    Fix Footstep Sync

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/CharacterSync.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Main Scripts/Multiplayer/CharacterSync.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58b37515ac71ebf8d663947f3fab56c237d08b4974da8046b6689b9352e21f68
-size 56289
+oid sha256:a3ab62475df630bb4f036979eed21ef850da6013ec3a29b06da5931bb2287a9d
+size 56291
