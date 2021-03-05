a4ed794d4f1864fb2618b00f56cd347dde08e29c
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2021-03-06 03:55:10 +0800

    Update LayerMask

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Tools/Scripts/_Helpers/Helper.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Tools/Scripts/_Helpers/Helper.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d8f78c839cc200f0eb5c640e4811ccf9ca9133675ae06fabb37ac75e4877641
-size 90230
+oid sha256:4ae53c49ae21a11d6cc11fdf81741a6de7df6a6c858fbb925dabbc38566b3b31
+size 90260
