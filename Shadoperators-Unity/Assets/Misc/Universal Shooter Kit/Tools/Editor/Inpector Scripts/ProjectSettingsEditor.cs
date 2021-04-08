3ebc3864657ed75788c5ec743994cff41eaba188
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2021-04-09 00:39:08 +0800

    Update Input Editor

--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Tools/Editor/Inpector Scripts/ProjectSettingsEditor.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Tools/Editor/Inpector Scripts/ProjectSettingsEditor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00f19fb94162df48d0d98707cacf9086661c3b44718152a59c83866e8cfab531
-size 24560
+oid sha256:0ec5a8272e82dc1ba5e0b3975535efa46cf58056a3e80c40352b9a0a1cfdeb7b
+size 26540
--- Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Tools/Scripts/ProjectSettings.cs
+++ Shadoperators-Unity/Assets/Misc/Universal Shooter Kit/Tools/Scripts/ProjectSettings.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d74b470f62aed9d0e389c9232b651da3a263500dcda8d0d37dc01f32d1757f74
+oid sha256:5e8b078d378e31c3043c00f7fe4c71643d75d41774e7b8e336a04ed94424816f
 size 1329
